af5621f6d12ed159c7967b92c87a8b3f5d8960b1
heronlancellot <heronlancellot@id.uff.br>
2022-12-25 20:10:52 -0300

    Add CSS Style Navbar

--- GPT3/frontend/src/components/navbar/Navbar.jsx
+++ GPT3/frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import Logo from '../../assets/logo.svg'
+
+// BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className='gpt3__navbar-links_logo'>
+          <img src={Logo} alt='logo'/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+
+        </div>
+        <div className='gpt3__navbar-sign'>
+          <p>Sign in</p>
+          <button type='button'>Sign up</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
