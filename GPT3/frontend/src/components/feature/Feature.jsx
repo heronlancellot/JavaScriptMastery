8e781f747ad377fea4617e36a132c82d26450c03
heronlancellot <heronlancellot@id.uff.br>
2023-01-22 15:37:41 -0300

    Feature: Feature section & WGPT3 section

--- GPT3/frontend/src/components/feature/Feature.jsx
+++ GPT3/frontend/src/components/feature/Feature.jsx
@@ -3,14 +3,14 @@ import './feature.css'
 
 const Feature = ({ title, text}) => {
   return (
-    <div className='gpt3__features-container__feature'>Features
+    <div className='gpt3__features-container__feature'>
       <div className='gpt3__features-container__feature-title'>
         <div />
         <h1>{title}</h1>
       </div>
 
-      <div className='gpt3__features-container_feature-text'>
-        {text}
+      <div className='gpt3__features-container__feature-text'>
+        <p>{text}</p>
       </div>
     </div>
   )
